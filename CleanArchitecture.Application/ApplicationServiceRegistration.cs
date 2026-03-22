85e507412961180b64d61f0a344eabb944540af5
Darkergg <luisgnio90@gmail.com>
2026-03-21 21:44:12 -0600

    add applicationServiceRegistration file for DI

--- CleanArchitecture.Application/ApplicationServiceRegistration.cs
+++ CleanArchitecture.Application/ApplicationServiceRegistration.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
+﻿using System.Reflection;
+using CleanArchitecture.Application.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanArchitecture.Application
 {
-    public class ApplicationServiceRegistration
+    public static class ApplicationServiceRegistration
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            //Old version dotNet 6
+            //services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            //dotNet 10
+            services.AddAutoMapper(cfg => { }, Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
             return services;
         }
